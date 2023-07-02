c1949e9c40fedd37e1f60868e89ee909795b2409
Amin Mollaei <79591523+aminmollaei@users.noreply.github.com>
2023-07-02 09:51:23 +0330

    https post request added

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,7 @@
 #include "client.hpp"
 #include "constants.hpp"
 
+
 HTTPClient *http;
 
 void createClient() {
@@ -17,4 +18,38 @@ int sendHttpPOSTrequest(WiFiClient client, const char* serverName, String httpRe
     debugln(http->getString());
     http->end();
     return httpResponseCode;
+}
+
+void setupHttpsClient()
+{
+  https = new WiFiClientSecure();
+  https.setCACert(test_root_ca);
+}
+
+int sendHttpsPOSTrequest(String requestBody)
+{
+  if (!client.connect(server, 443))
+  {
+    Serial.println("Connection failed!");
+    return -1;
+  }
+
+client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Content-Type: application/json\r\n" +
+               "Content-Length: " + requestBody.length() + "\r\n\r\n" +
+               requestBody + "\r\n");
+
+               /* HTTPS read
+                while (client.connected()) {
+                  String line = client.readStringUntil('\n');
+                  if (line == "\r") {
+                    break;
+                  }
+                  Serial.println(line);
+                 }
+
+                  String line = client.readStringUntil('\n');
+               */
+
 }
